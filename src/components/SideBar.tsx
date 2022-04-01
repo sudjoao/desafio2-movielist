17f4a1119726057cb49fa566723ae912a201f381
sudjoao <isudjoao@gmail.com>
2022-04-01 17:59:35 -0300

    Tranform app code in components

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,29 @@
-export function SideBar() {
-  // Complete aqui
+import { GenreResponseProps } from "../App";
+import { Button } from "./Button";
+
+import '../styles/sidebar.scss';
+
+interface ISidebarProps {
+  genres: GenreResponseProps[]
+  onClick: Function
+  selectedGenreId: number
+}
+
+export function SideBar(props: ISidebarProps) {
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+      <div className="buttons-container">
+        {props.genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => props.onClick(genre.id)}
+            selected={props.selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
